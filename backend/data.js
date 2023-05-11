3b88f46c49b0c5313e644dd9d4fce4e3c7c49604
fatima2123 <fshahzad2123@gmail.com>
2023-05-12 00:03:03 +0500

    secure

--- backend/data.js
+++
@@ -1,5 +0,0 @@
-//mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/<dbname>?retryWrites=true&w=majority
-module.exports={
-    mongoURL:"mongodb+srv://fatimaShahzad:Patanahi@cluster0.oyepgzu.mongodb.net/notebook?retryWrites=true&w=majority",
-    secretKey:"Mynameisfatimashahxad",
-}
